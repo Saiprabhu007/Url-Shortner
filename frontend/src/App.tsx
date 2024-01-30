961f7ad40945dcac5523a21e0e8a108321cef3c3
Saiprabhu007 <saiprabhu.dandanayak@zemosolabs.com>
2024-01-30 20:08:54 +0530

    feat : 🚧 Added Frontend Scaffolding

---
+++ frontend/src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const App = () => {
+  return <h1>bootcamp-125-checkr</h1>;
+};
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname,'src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  resolve:{
+    extensions: ['.tsx','.ts','.js'],
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|jp2|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    open: true
+  }
+};
